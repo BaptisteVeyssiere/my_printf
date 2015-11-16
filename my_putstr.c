36b42dd16b5b0de9056a798a03a227b6cd41bbfe
Baptiste veyssiere <baptiste.veyssiere@epitech.eugit config --global user.email baptiste.veyssiere@epitech.eugit config --global user.name veyssi_bgit commit -am 1>
2015-11-16 19:51:18 +0100

    com

--- my_putstr.c
+++ my_putstr.c
@@ -5,14 +5,16 @@
 ** Login   <VEYSSI_B@epitech.net>
 ** 
 ** Started on  Sat Nov  7 21:01:49 2015 Baptiste veyssiere
-** Last update Mon Nov 16 11:25:18 2015 Baptiste veyssiere
+** Last update Mon Nov 16 19:09:24 2015 Baptiste veyssiere
 */
 
 void	my_putstr(char *s, int *length)
 {
   if (s != 0)
     while (*s != 0)
-      my_putchar(*s++, length);
+      {
+	my_putchar(*s++, length);
+      }
   else
     my_putstr("(null)",length);
 }
--- signed_unsigned.c
+++ signed_unsigned.c
@@ -5,7 +5,7 @@
 ** Login   <VEYSSI_B@epitech.net>
 ** 
 ** Started on  Mon Nov  9 17:00:18 2015 Baptiste veyssiere
-** Last update Sun Nov 15 21:05:23 2015 Baptiste veyssiere
+** Last update Mon Nov 16 19:47:50 2015 Baptiste veyssiere
 */
 
 #include <stdarg.h>
@@ -13,17 +13,17 @@
 void    case_short(va_list ap, const char *list, int *i, int *length)
 {
   if (list[*i + 1] == 'i' || list[*i + 1] == 'd')
-    case_signed_short(ap, list, i);
+    case_signed_short(ap, list, i, length);
   if (list[*i + 1] == 'u')
-    case_unsigned_short(ap, list, i);
+    case_unsigned_short(ap, list, i, length);
   *i += 1;
 }
 
 void    case_long(va_list ap, const char *list, int *i, int *length)
 {
   if (list[*i + 1] =='i' || list[*i + 1] == 'd')
-    case_signed_long(ap, list, i);
+    case_signed_long(ap, list, i, length);
   if (list[*i + 1] =='u')
-    case_unsigned_long(ap, list, i);
+    case_unsigned_long(ap, list, i, length);
   *i += 1;
 }
